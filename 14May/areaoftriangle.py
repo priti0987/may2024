2ebc0fe5b6199a04b9be37b2cb3ed72402fdabf3
priti <pritibhushanf@gmail.com>
2024-05-14 07:54:15 +0530

    14May

---
+++ 14May/areaoftriangle.py
@@ -0,0 +1,4 @@
+def areaOftriangle(base,height):
+    return base*height/2
+
+print("Area of given triangle is ",areaOftriangle(2,5))
---
+++ 14May/arithmaticOperation.py
@@ -0,0 +1,16 @@
+#addition, division, subtraction, multiplication
+
+def addition(num1,num2):
+    return num1 +num2
+
+def subtraction(num1,num2):
+    return num1 - num2
+def multiplication(num1,num2):
+    return num1  * num2
+def division(num1,num2):
+    return num1 //num2
+
+print("Addtion of given two numbers is :",addition(5,3))
+print("multiplication of given two numbers is :",multiplication(2,3))
+print("subtraction of given two numbers is :",subtraction(4,3))
+print("division of given two numbers is :",division(6,3))
